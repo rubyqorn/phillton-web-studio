010a0c8733403742beec44cda21c974566dd0e7b
Anton Hideger <antonhideger1337@gmail.com>
2019-06-23 17:16:30 +0300

    make a service page

---
+++ public/services.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Phillton - прогрессивная веб-студия</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Lalezar|Montserrat|Raleway&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+	
+	<!-- Navigation -->
+
+	<nav>
+		<div class="navbar navbar-dark bg-primary navbar-expand-lg">
+			<a href="/" class="navbar-brand">Phillton</a>
+			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="navbar-item"><a href="/" class="nav-link">Home</a></li>
+					<li class="navbar-item"><a href="/works.php" class="nav-link">Works</a></li>
+					<li class="navbar-item"><a href="/services.php" class="nav-link">Services</a></li>
+					<li class="navbar-item"><a href="/about.php" class="nav-link">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<section id="services">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-md-12 col-sm-12">
+					<h2 class="text-center font-weight-bold">Наши услуги</h2>
+					<hr class="blue-hr">
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="card bg-dark">
+						<a href="/"><img src="/img/site-dev.png" alt="" class="w-100 card-img"></a>
+						<div class="card-img-overlay">
+							<h2 class=" text-center">
+								<a href="/" class="nav-link text-dark font-weight-bold">Разработка сайтов</a>
+							</h2>
+							<p class="card-text font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas, non.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="card bg-dark">
+						<img src="/img/design.png" alt="" class="w-100 card-img">
+						<div class="card-img-overlay">
+							<h2 class="text-center">
+								<a href="/" class="nav-link text-dark font-weight-bold">Редизайн сайта</a>
+							</h2>
+							<p class="card-text font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Praesentium, in!</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="card bg-dark">
+						<img src="/img/seo.png" alt="" class="w-100 card-img">
+						<div class="card-img-overlay">
+							<h2 class="text-center">
+								<a href="/" class="nav-link text-dark font-weight-bold">Поисковая оптимизация(SEO)</a>
+							</h2>
+							<p class="card-text font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, dignissimos!</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="card bg-dark">
+						<img src="/img/ads.png" alt="" class="w-100 card-img">
+						<div class="card-img-overlay">
+							<h2 class="text-center">
+								<a href="/" class="nav-link text-dark font-weight-bold">Контекстная реклама</a>
+							</h2>
+							<p class="card-text font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis, dolore.</p>
+						</div>
+					</div>
+				</div>
+
+				<div class="col-lg-6 col-md-6 col-sm-12">
+					<div class="card bg-dark">
+						<img src="/img/landing.png" alt="" class="w-100 card-img">
+						<div class="card-img-overlay">
+							<h2 class="text-center">
+								<a href="/" class="nav-link text-dark font-weight-bold">Создание landing page</a>
+							</h2>	
+							<p class="card-text font-weight-bold">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni, illum.</p>
+						</div>
+					</div>
+				</div>
+
+			</div>
+		</div>
+	</section>
+
+	<!-- Footer -->
+	
+	<footer>
+		<div class="row justify-content-center">
+			<div class="col-lg-12 col-md-12 col-sm-12">
+				<div class="social-networks text-center">
+					<a href="/"><i class="fab fa-vk fa-lg text-white"></i></a>
+					<a href="/"><i class="fab fa-instagram fa-lg text-white"></i></a>
+					<a href="/"><i class="fab fa-telegram-plane fa-lg text-white"></i></a>
+				</div>
+				<p class="text-white font-weight-bold text-center">&copy; Copyright Phillton 2014-2019</p>
+				<p class="text-white font-weight-bold text-center">Designed by Anton Hideger</p>
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="/js/app.js"></script>
+</body>
+</html>
