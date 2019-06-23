1fffefb3f27934f340653f167b19a001971c7ef9
Anton Hideger <antonhideger1337@gmail.com>
2019-06-23 19:04:14 +0300

    create about page

---
+++ public/about.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Phillton - прогрессивная веб-студия</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Lalezar|Montserrat|Raleway&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+	
+	<!-- Navigation -->
+
+	<nav>
+		<div class="navbar navbar-dark bg-primary navbar-expand-lg">
+			<a href="/" class="navbar-brand">Phillton</a>
+			<button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarContent">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+			<div class="collapse navbar-collapse" id="navbarContent">
+				<ul class="navbar-nav ml-auto">
+					<li class="navbar-item"><a href="/" class="nav-link">Home</a></li>
+					<li class="navbar-item"><a href="/works.php" class="nav-link">Works</a></li>
+					<li class="navbar-item"><a href="/services.php" class="nav-link">Services</a></li>
+					<li class="navbar-item"><a href="/about.php" class="nav-link">About</a></li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+
+	<!-- About section -->
+
+	<section id="about">
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-12 col-mg-12 col-sm-12">
+					<h2 class="text-center font-weight-bold">Больше информации о нас</h2>
+					<hr class="blue-hr">
+				</div>
+				<div class="col-lg-12 col-md-12 col-sm-12 shadow-box">
+					
+					<div class="col-lg-5 col-md-5 col-sm-12 float-left">
+						<h3 class="text-center">Стек технологий</h3>
+						<hr class="blue-hr">
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">HTML/CSS</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-dark" role="progress" style="width: 95%">95%</div>
+							</div>
+						</div>
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">Javascipt</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-success" role="progress" style="width: 85%">85%</div>
+							</div>
+						</div>
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">jQuery</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-warning" role="progress" style="width: 90%">90%</div>
+							</div>
+						</div>
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">Bootstrap 4</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-info" role="progress" style="width: 95%">95%</div>
+							</div>
+						</div>
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">PHP</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-primary" role="progress" style="width: 90%">90%</div>
+							</div>
+						</div>
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">Laravel</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-danger" role="progress" style="width: 80%">80%</div>
+							</div>
+						</div>
+
+						<div class="about-item">
+							<h4 class="text-center text-dark font-weight-bold">MySQL</h4>
+							<div class="progress">
+								<div class="progress-bar progress-bar-striped progress-bar-animated bg-secondary" role="progress" style="width: 90%">90%</div>
+							</div>
+						</div>
+
+					</div>
+
+					<div class="col-lg-6 col-md-6 col-sm-12 float-right">
+						<h3 class="text-center">О нашей команде</h3>
+						<hr class="blue-hr">
+						<p class="text-left">
+							Мы - объединение энтузиастов, стремящихся к достижению непокоренных высот в области web, в поиске новых решений и использования их на практике в своих эксклюзивных проектах..
+						</p>
+						<p class="text-left">
+							Phillton - значит «новый ритм», новый ритм вашего бизнеса. Мы как свежий глоток воздуха, который, пройдя через призму нашего опыта и профессионализма, позволит выйти на свет новому уникальному проекту, нацеленному на успех.
+						</p>
+						<p class="text-left">
+							Сегодня мы - одни из лидеров в направлении веб-разработок, веб-дизайна и продвижения бизнеса, так как мы точно знаем, что необходимо вашим клиентам. Мы относимся к каждому клиенту как к члену команды, члену семьи, в которой царит дружеская атмосфера, способствующая поиску наиболее точных и правильных решений.
+						</p>
+						<p class="text-left">
+							Наша миссия - наполнить Интернет исключительно качественными веб-сайтами, оставив позади устаревшие ресурсы, которые более не приносят пользы. Наша цель - обеспечить развитие бизнеса наших клиентов, сделать это в кратчайшие сроки и с наибольшей эффективностью.
+						</p>
+						<p class="text-left">
+							Мы уделяем особое внимание качеству предоставляемых услуг. К нам часто приходят клиенты, имеющие мотивированное недовольство от проектов, разработанных фрилансерами или студиями, которые смотрят на качество своих услуг сквозь пальцы. Нам бесконечно неприятно, что многие владельцы сайтов имеют подобные проблемы. Именно во избежание подобных негативных впечатлений, мы открыли отдел контроля качества, который проверяет проекты, разработанные нами. Материалы, валидность верстки, возможные уязвимости в программах - все находится под пристальным взглядом службы контроля качества. На кону репутация и чистая совесть перед нашими уважаемыми клиентами.
+						</p>
+					</div>
+
+				</div>
+			</div>	
+		</div>
+	</section>
+
+	<!-- Footer -->
+	
+	<footer>
+		<div class="row justify-content-center">
+			<div class="col-lg-12 col-md-12 col-sm-12">
+				<div class="social-networks text-center">
+					<a href="/"><i class="fab fa-vk fa-lg text-white"></i></a>
+					<a href="/"><i class="fab fa-instagram fa-lg text-white"></i></a>
+					<a href="/"><i class="fab fa-telegram-plane fa-lg text-white"></i></a>
+				</div>
+				<p class="text-white font-weight-bold text-center">&copy; Copyright Phillton 2014-2019</p>
+				<p class="text-white font-weight-bold text-center">Designed by Anton Hideger</p>
+			</div>
+		</div>
+	</footer>
+
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<script src="/js/app.js"></script>
+</body>
+</html>
